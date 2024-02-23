905f4ff61c4e1543e72395d69273c3d131be2a83
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-02-23 12:14:26 +0530

    category Controllers and routes done

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ connectDB();
 app.use(express.json());
 
 
+// routes
+
+app.use('/categories', require('./routes/categoryRoutes'));
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
